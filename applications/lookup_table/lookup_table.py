0bbd8c94a3be8c12044561e14f37c0153a91866d
Daniel Mattox <dmattox10@gmail.com>
2020-09-28 19:22:53 -0400

    Faster lookup table, I can make it better

--- applications/lookup_table/lookup_table.py
+++ applications/lookup_table/lookup_table.py
@@ -1,3 +1,5 @@
+import random
+import math
 # Your code here
 
 
@@ -9,18 +11,33 @@ def slowfun_too_slow(x, y):
 
     return v
 
+
+cache = {}
+
+
 def slowfun(x, y):
     """
     Rewrite slowfun_too_slow() in here so that the program produces the same
     output, but completes quickly instead of taking ages to run.
     """
     # Your code here
+    if (x, y) in cache:
 
+        return cache[(x, y)]
+
+    else:
+        v = math.pow(x, y)
+        v = math.factorial(v)
+        v //= (x + y)
+        v %= 982451653
+
+        cache[(x, y)] = v
+
+        return v
 
 
 # Do not modify below this line!
-
 for i in range(50000):
     x = random.randrange(2, 14)
     y = random.randrange(3, 6)
-    print(f'{i}: {x},{y}: {slowfun(x, y)}')
+    print(f'{i}: {x},{y}: {slowfun_too_slow(x, y)}')
