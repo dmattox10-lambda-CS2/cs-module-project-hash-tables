dfc9a84575ec32200a43487c6bf45b108014e2ea
Daniel Mattox <dmattox10@gmail.com>
2020-09-30 12:48:36 -0400

    Finished expensive sequence

---
+++ applications/fib.py
@@ -0,0 +1,15 @@
+cache = {}
+
+
+def fib(n):
+    if n <= 1:
+        return n
+
+    if n not in cache:
+        cache[n] = fib(n-1) + fib(n-2)
+
+    return cache[n]
+
+
+print(fib(35))
+print(f'{cache}')
